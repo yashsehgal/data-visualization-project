b18806c5b58981a793b628536469bd052fa7accb
yashsehgal <yashsehgal.work@gmail.com>
2020-07-16 01:23:23 +0530

    new file to fetch data

---
+++ python-codebase/fetch_data.py
@@ -0,0 +1,15 @@
+'''
+  This code module is to fetch the data in such a format which can be used
+  for data analysis and visualization methods.
+  
+  fetch_data.py is the module which every module will use to get some amount of dataset and values.
+'''
+
+# TODO: To initialize a useful setup for data warehousing.
+# TODO: To fetch data from the CSV file and store each type in different containers.
+# TODO: To make getters for various features and modules
+
+# ? Check all the features from the CSV files
+# ? Manage all the methods and review every feature after performance testing.
+# ? Optimise all the features and methods throughout the time of development.
+
