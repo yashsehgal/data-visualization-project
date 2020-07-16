f28901630c1e9fbcc51f2f73b52b6cb631508e47
yashsehgal <yashsehgal.work@gmail.com>
2020-07-16 22:52:49 +0530

    some folder-packages are renamed

---
+++ analysis/gender_wise_placement_analysis.py
@@ -0,0 +1,19 @@
+'''
+  This modules has data visualization methods to analyse the placements on the
+  basis of gender. 
+  This module is linked with the support module from python-codebase/fetch_data.py
+  This module is part of the Development process of this project.
+'''
+
+'''
+  # TODO: To fetch the genders and placement index from the fetching module
+  # TODO: To generate graphs according to the dataset 
+  # ? To manage all the methods and functions for graphs and value handlers
+'''
+
+class GenederWisePlacementAnalysis:
+  def __init__(self, activation = False):
+    self.activation = activation
+    
+  def generateGraph(self):
+     
