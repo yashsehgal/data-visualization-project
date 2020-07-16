bd8ea20f328378628073970b555bf2c913adac5f
yashsehgal <yashsehgal.work@gmail.com>
2020-07-16 19:01:47 +0530

    fetching methods are added for all the labels

--- python-codebase/fetch_data.py
+++ python-codebase/fetch_data.py
@@ -37,5 +37,38 @@ class FetchData:
   def fetch_data_from_file(self):
     # opening the csv file to fetch data
     datafile = pandas.read_csv("datasets/dataset.csv")
+    self.serial_number = datafile[datafile.sl_no]
+    self.gender_list = datafile[datafile.gender]
+    self.number_of_males = len(datafile[datafile.gender == 'M'])
+    self.number_of_females = len(datafile[datafile.gender == 'F'])
+    self.secondary_school_percentage = datafile[datafile.ssc_p]
+    self.secondary_school_board = datafile[datafile.ssc_b]
+    self.number_of_central_board_secondary = len(datafile[datafile.ssc_b == "Central"])
+    self.number_of_other_board_secondary = len(datafile[datafile.ssc_b == "Others"])
+    self.high_school_percentage = datafile[datafile.hsc_p]
+    self.high_school_board = datafile[datafile.hsc_b]
+    self.number_of_central_board_high = len(datafile[datafile.hsc_b == "Central"])
+    self.number_of_other_board_high = len(datafile[datafile.hsc_b == "Others"])
+    self.high_school_subjects = datafile[datafile.hsc_s]
+    self.high_school_arts = len(datafile[datafile.hsc_s == "Arts"])
+    self.high_school_science = len(datafile[datafile.hsc_s == "Science"])
+    self.high_school_commerce = len(datafile[datafile.hsc_s == "Commerce"])
+    self.college_degree = datafile[datafile.degree_t]
+    self.number_of_commerce_management_degree = len(datafile[datafile.degree_t == "Comm&Mgmt"])
+    self.number_of_science_tech_degree = len(datafile[datafile.degree_t == "Sci&Tech"])
+    self.number_of_other_degree = len(datafile[datafile.degree_t == "Others"])
+    self.college_marks = datafile[datafile.degree_p]
+    self.work_experience = datafile[datafile.workexp]
+    self.number_of_no_workexp = len(datafile[datafile.workexp == "No"])
+    self.number_of_yes_workexp = len(datafile[datafile.workexp == "Yes"])
+    self.emp_test_percentage = datafile[datafile.etest_p]
+    self.students_specialization = datafile[datafile.specialisation]
+    self.number_of_mkt_hr_spec = len(datafile[datafile.specialisation == "Mkt&HR"])
+    self.number_of_mkt_fin_spec = len(datafile[datafile.specialisation == "Mkt&Fin"])
+    self.mba_percentage = datafile[datafile.mba_p]
+    self.student_placement_status = datafile[datafile.status]
+    self.number_of_student_placed = len(datafile[datafile.status == "Placed"])
+    self.number_of_student_not_placed = len(datafile[datafile.status == "Not Placed"])
+    self.salary_index = datafile[datafile.salary]
     
     
