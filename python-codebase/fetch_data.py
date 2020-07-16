ff13f0e2ecd0930f3c6e8ddc981e94b781315d21
yashsehgal <yashsehgal.work@gmail.com>
2020-07-16 22:39:36 +0530

    automatic fetching method is added to all the functions used for getting the datavalues from the dataset

--- python-codebase/fetch_data.py
+++ python-codebase/fetch_data.py
@@ -78,90 +78,119 @@ class FetchData:
   '''
   
   def get_serial_numbers(self):
+    self.fetch_data_from_file()
     return self.serial_number
   
   def get_gender_list(self):
+    self.fetch_data_from_file()
     return self.gender_list
   
   def get_number_of_males(self):
+    self.fetch_data_from_file()
     return self.number_of_males
   
   def get_number_of_females(self):
+    self.fetch_data_from_file()
     return self.number_of_females
   
   def get_secondary_school_percentage_list(self):
+    self.fetch_data_from_file()
     return self.secondary_school_percentage
   
   def get_number_of_central_board_in_secondary_school(self):
+    self.fetch_data_from_file()
     return self.number_of_central_board_secondary
   
   def get_number_of_other_boards_in_secondary_school(self):
+    self.fetch_data_from_file()
     return self.number_of_other_board_secondary
   
   def get_high_school_percentage_list(self):
+    self.fetch_data_from_file()
     return self.high_school_percentage
   
   def get_number_of_central_board_in_high_school(self):
+    self.fetch_data_from_file()
     return self.number_of_central_board_high
   
   def get_number_of_other_boards_in_high_school(self):
+    self.fetch_data_from_file()
     return self.number_of_other_board_high
   
   def get_high_school_subjects(self):
+    self.fetch_data_from_file()
     return self.high_school_subjects
   
   def get_number_of_arts_in_high_school(self):
+    self.fetch_data_from_file()
     return self.high_school_arts
   
   def get_number_of_commerce_in_high_school(self):
+    self.fetch_data_from_file()
     return self.high_school_commerce
   
   def get_number_of_science_in_high_school(self):
+    self.fetch_data_from_file()
     return self.high_school_science
   
   def get_college_degree_list(self):
+    self.fetch_data_from_file()
     return self.college_degree
   
   def get_number_of_commerce_management_degree(self):
+    self.fetch_data_from_file()
     return self.number_of_commerce_management_degree
   
   def get_number_of_science_tech_degree(self):
+    self.fetch_data_from_file()
     return self.number_of_science_tech_degree
   
   def get_number_of_other_degree(self):
+    self.fetch_data_from_file()
     return self.number_of_other_degree
   
   def get_college_marks_list(self):
+    self.fetch_data_from_file()
     return self.college_marks
   
   def get_work_experience_list(self):
+    self.fetch_data_from_file()
     return self.work_experience
   
   def get_number_of_no_work_experience(self):
+    self.fetch_data_from_file()
     return self.number_of_no_workexp
   
   def get_number_of_yes_work_experience(self):
+    self.fetch_data_from_file()
     return self.number_of_yes_workexp
   
   def get_students_specialization_list(self):
+    self.fetch_data_from_file()
     return self.students_specialization
   
   def get_students_spec_in_mkt_hr(self):
+    self.fetch_data_from_file()
     return self.number_of_mkt_hr_spec
   
   def get_students_spec_in_mkt_fin(self):
+    self.fetch_data_from_file()
     return self.number_of_mkt_fin_spec
   
   def get_mba_percentage_list(self):
+    self.fetch_data_from_file()
     return self.mba_percentage
   
   def get_student_placement_status_list(self):
+    self.fetch_data_from_file()
     return self.student_placement_status
   
   def get_number_of_placed_students(self):
+    self.fetch_data_from_file()
     return self.number_of_student_placed
   
   def get_number_of_non_placed_students(self):
+    self.fetch_data_from_file()
     return self.number_of_student_not_placed
   
   def get_salary_index_list(self):
